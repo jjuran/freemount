7944cfdd8c3c7be76eeb58228fd05772e0a5cc59
Josh Juran <jjuran@metamage.com>
2013-01-27 13:59:40 -0800

    write_in_full:  Add tests

---
+++ t/write_in_full.cc
@@ -0,0 +1,126 @@
+/*
+	write_in_full.cc
+	----------------
+*/
+
+// POSIX
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+
+// freemount
+#include "freemount/write_in_full.hh"
+
+// tap-out
+#include "tap/check.hh"
+#include "tap/test.hh"
+
+
+static const unsigned n_tests = 3 * 8;
+
+
+using freemount::write_in_full;
+using freemount::failed_write;
+
+using tap::ok_if;
+
+
+static sig_atomic_t caught_sigalrm;
+static sig_atomic_t caught_sigpipe;
+
+static int fd_to_close = -1;
+
+
+static void sigalrm_handler( int )
+{
+	if ( caught_sigalrm++ )
+	{
+		CHECK( close( fd_to_close ) );
+		
+		fd_to_close = -1;
+	}
+	
+	CHECK( alarm( 1 ) );
+}
+
+static void sigpipe_handler( int )
+{
+	++caught_sigpipe;
+}
+
+static void set_fd_nonblocking( int fd )
+{
+	int flags = CHECK( fcntl( fd, F_GETFL, 0 ) );
+	
+	CHECK( fcntl( fd, F_SETFL, flags | O_NONBLOCK ) );
+}
+
+static void pipe_buffer_overrun( size_t buffer_size, bool nonblocking, bool closing_writer )
+{
+	char* local_buffer = (char*) ::operator new( buffer_size );
+	
+	int fds[ 2 ];
+	
+	CHECK( pipe( fds ) );
+	
+	fd_to_close = fds[ closing_writer ];
+	
+	if ( nonblocking )
+	{
+		set_fd_nonblocking( fds[1] );
+	}
+	
+	int errnum = 0;
+	
+	caught_sigalrm = 0;
+	caught_sigpipe = 0;
+	
+	CHECK( (long) signal( SIGALRM, &sigalrm_handler ) );
+	CHECK( (long) signal( SIGPIPE, &sigpipe_handler ) );
+	
+	CHECK( alarm( 1 ) );
+	
+	try
+	{
+		for ( ;; )
+		{
+			write_in_full( fds[1], local_buffer, buffer_size );
+		}
+	}
+	catch ( const failed_write& error )
+	{
+		errnum = error.errnum;
+	}
+	
+	alarm( 0 );
+	
+	ok_if( caught_sigalrm == 2 );
+	
+	ok_if( caught_sigpipe == !closing_writer );
+	
+	ok_if( errnum == (closing_writer ? EBADF : EPIPE) );
+	
+	close( fds[0] );
+	close( fds[1] );
+	
+	::operator delete( local_buffer );
+}
+
+int main( int argc, char** argv )
+{
+	tap::start( "write_in_full", n_tests );
+	
+	for ( int i = 0;  i < 8;  ++i )
+	{
+		const size_t buffer_size = i < 4 ? 512 : 512 * 1024;
+		
+		const bool nonblocking    = i & 2;
+		const bool closing_writer = i & 1;
+		
+		pipe_buffer_overrun( buffer_size, nonblocking, closing_writer );
+	}
+	
+	return 0;
+}
+
