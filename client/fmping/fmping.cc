488e23fcbfda79ee6f9091cc91aa97d1a4196291
Josh Juran <jjuran@metamage.com>
2013-02-07 20:22:08 -0800

    fmping:  Use threads

--- client/fmping/fmping.cc
+++ client/fmping/fmping.cc
@@ -4,6 +4,7 @@
 */
 
 // POSIX
+#include <pthread.h>
 #include <unistd.h>
 #include <sys/socket.h>
 
@@ -18,6 +19,8 @@
 
 using namespace freemount;
 
+
+static int event_loop_result;
 
 static int fragment_handler( void* that, const fragment_header& fragment )
 {
@@ -34,16 +37,27 @@ static int fragment_handler( void* that, const fragment_header& fragment )
 	return 0;
 }
 
+static void* event_loop_start( void* arg )
+{
+	data_receiver r( &fragment_handler, NULL );
+	
+	event_loop_result = run_event_loop( r, STDIN_FILENO );
+	
+	return NULL;
+}
+
 int main( int argc, char** argv )
 {
+	pthread_t event_loop;
+	
+	pthread_create( &event_loop, NULL, &event_loop_start, NULL );
+	
 	send_empty_fragment( STDOUT_FILENO, frag_ping );
 	
 	shutdown( STDOUT_FILENO, SHUT_WR );
 	
-	data_receiver r( &fragment_handler, NULL );
+	pthread_join( event_loop, NULL );
 	
-	int looped = run_event_loop( r, STDIN_FILENO );
-	
-	return looped != 0;
+	return event_loop_result != 0;
 }
 
