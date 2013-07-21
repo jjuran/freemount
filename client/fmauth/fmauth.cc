b0b4fe2d3b08ab7416605b86cfabb92cd3ecd4eb
Josh Juran <jjuran@metamage.com>
2013-07-21 01:58:29 -0700

    fmauth:  Speak freemount protocol on fds 6 and 7
    Using 0 and 1 conflicts with user interaction, whereas use of 6 and 7 is
    a precedent set by tcpclient.

--- client/fmauth/fmauth.cc
+++ client/fmauth/fmauth.cc
@@ -18,12 +18,16 @@
 using namespace freemount;
 
 
+const int protocol_in  = 6;
+const int protocol_out = 7;
+
+
 static int fragment_handler( void* that, const fragment_header& fragment )
 {
 	switch ( fragment.type )
 	{
 		case frag_eom:
-			write( STDERR_FILENO, "auth ok\n", 8 );
+			write( STDOUT_FILENO, "auth ok\n", 8 );
 			exit( 0 );
 			break;
 		
@@ -36,11 +40,11 @@ static int fragment_handler( void* that, const fragment_header& fragment )
 
 int main( int argc, char** argv )
 {
-	send_empty_request( STDOUT_FILENO, req_auth );
+	send_empty_request( protocol_out, req_auth );
 	
 	data_receiver r( &fragment_handler, NULL );
 	
-	int looped = run_event_loop( r, STDIN_FILENO );
+	int looped = run_event_loop( r, protocol_in );
 	
 	return looped != 0;
 }
