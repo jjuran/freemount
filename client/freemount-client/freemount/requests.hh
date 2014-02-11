e6df5c5b9d97f47f1ad8827c5e57cf6accbe9625
Josh Juran <jjuran@metamage.com>
2014-02-11 03:06:28 -0800

    freemount-client:  Add send_*_request()

---
+++ client/freemount-client/freemount/requests.hh
@@ -0,0 +1,38 @@
+/*
+	freemount/requests.hh
+	---------------------
+*/
+
+#ifndef FREEMOUNT_REQUESTS_HH
+#define FREEMOUNT_REQUESTS_HH
+
+// Standard C
+#include <stdint.h>
+
+// freemount
+#include "freemount/fragment.hh"
+
+
+namespace freemount
+{
+	
+	void send_path_request( int fd, const char* path, uint32_t size, uint8_t r_type, uint8_t r_id = 0 );
+	
+	inline void send_stat_request( int fd, const char* path, uint32_t size, uint8_t r_id = 0 )
+	{
+		send_path_request( fd, path, size, req_stat, r_id );
+	}
+	
+	inline void send_list_request( int fd, const char* path, uint32_t size, uint8_t r_id = 0 )
+	{
+		send_path_request( fd, path, size, req_list, r_id );
+	}
+	
+	inline void send_read_request( int fd, const char* path, uint32_t size, uint8_t r_id = 0 )
+	{
+		send_path_request( fd, path, size, req_read, r_id );
+	}
+	
+}
+
+#endif
