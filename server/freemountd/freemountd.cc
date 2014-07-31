0245507a272e5c145c3de8baa6c3cfc88d175f61
Josh Juran <jjuran@metamage.com>
2014-07-31 00:52:45 -0700

    freemountd:  Rewrite option processing

--- server/freemountd/freemountd.cc
+++ server/freemountd/freemountd.cc
@@ -6,11 +6,12 @@
 // POSIX
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/uio.h>
 
 // Standard C
 #include <stdlib.h>
-#include <string.h>
+
+// command
+#include "command/get_option.hh"
 
 // vfs
 #include "vfs/node.hh"
@@ -25,8 +26,25 @@
 #include "freemount/session.hh"
 
 
+using namespace command::constants;
 using namespace freemount;
 
+
+enum
+{
+	Option_quiet = 'q',
+	
+	Option_last_byte = 255,
+	
+	Option_root,
+};
+
+static command::option options[] =
+{
+	{ "quiet", Option_quiet },
+	{ "root",  Option_root, Param_required },
+	{ NULL }
+};
 
 static const char* the_native_root_directory = "/var/freemount";
 
@@ -43,128 +61,32 @@ static const vfs::node& root()
 
 #define ARRAYLEN( array )  (sizeof array / sizeof array[0])
 
-static void bad_usage( const char* text, size_t text_size, const char* arg )
-{
-	const iovec iov[] =
-	{
-		{ (void*) text, text_size    },
-		{ (void*) arg, strlen( arg ) },
-		{ (void*) STR_LEN( "\n" )    },
-	};
-	
-	writev( STDERR_FILENO, iov, ARRAYLEN( iov ) );
-	
-	exit( 2 );
-}
-
-#define BAD_USAGE( text, arg )  bad_usage( STR_LEN( text ": " ), arg )
-
-static const char* find_char( const char* begin, char c )
-{
-	while ( *begin != '\0'  &&  *begin != c )
-	{
-		++begin;
-	}
-	
-	return begin;
-}
-
-static bool option_matches( const char*  option,
-                            size_t       option_size,
-                            const char*  name,
-                            size_t       name_size )
-{
-	return option_size == name_size  &&  memcmp( option, name, name_size ) == 0;
-}
-
-#define OPTION_MATCHES( option, size, name )  option_matches( option, size, STR_LEN( name ) )
-
 static char* const* get_options( char* const* argv )
 {
-	if ( *argv == NULL )
-	{
-		// POSIX says we have to check for this
-		return argv;
-	}
+	++argv;  // skip arg 0
 	
-	while ( const char* arg = *++argv )
+	short opt;
+	
+	while ( (opt = command::get_option( &argv, options )) )
 	{
-		if ( arg[0] == '-' )
+		switch ( opt )
 		{
-			if ( arg[1] == '\0' )
-			{
-				// An "-" argument is not an option and means /dev/fd/0
-				break;
-			}
-			
-			if ( arg[1] == '-' )
-			{
-				// long option or "--"
-				
-				const char* option = arg + 2;
-				
-				if ( *option == '\0' )
-				{
-					++argv;
-					break;
-				}
-				
-				const char* equals = find_char( option, '=' );
-				
-				const size_t size = equals - option;
-				
-				if ( OPTION_MATCHES( option, size, "root" ) )
-				{
-					if ( *equals == '\0' )
-					{
-						++argv;
-						
-						if ( *argv == NULL )
-						{
-							BAD_USAGE( "Argument required", arg );
-						}
-						
-						the_native_root_directory = *argv;
-					}
-					else
-					{
-						const char* param = equals + 1;
-						
-						if ( param[0] == '\0' )
-						{
-							BAD_USAGE( "Invalid option", arg );
-						}
-						
-						the_native_root_directory = param;
-					}
-					
-					continue;
-				}
-				
-				BAD_USAGE( "Unknown option", arg );
-			}
-			
-			// short option
-			
-			const char* opt = arg + 1;
-			
-			if ( opt[0] == 'q' )
-			{
+			case Option_quiet:
 				int dev_null;
 				dev_null = open( "/dev/null", O_WRONLY );
 				
 				dup2( dev_null, STDERR_FILENO );
 				
 				close( dev_null );
-				
-				continue;
-			}
+				break;
 			
-			BAD_USAGE( "Unknown option", arg );
+			case Option_root:
+				the_native_root_directory = command::global_result.param;
+				break;
+			
+			default:
+				abort();
 		}
-		
-		// not an option
-		break;
 	}
 	
 	return argv;
