1ab7631cd55a1f430bb6b59ce438b86878a657a1
Josh Juran <jjuran@metamage.com>
2016-05-17 15:45:11 -0400

    freemount:  Add send_read_ack() function

---
+++ common/freemount/send_ack.hh
@@ -0,0 +1,17 @@
+/*
+	freemount/send_ack.hh
+	---------------------
+*/
+
+#ifndef FREEMOUNT_SENDACK_HH
+#define FREEMOUNT_SENDACK_HH
+
+
+namespace freemount
+{
+	
+	void send_read_ack( int fd, unsigned n_bytes );
+	
+}
+
+#endif
