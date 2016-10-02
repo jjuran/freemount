0be88fa1d24c10c6734c62ca32d71270455d54a5
Josh Juran <jjuran@metamage.com>
2016-10-01 21:59:46 -0400

    fset:  Rewrite to use synced_put()

--- client/fset/fset.cc
+++ client/fset/fset.cc
@@ -16,14 +16,9 @@
 // unet-connect
 #include "unet/connect.hh"
 
-// freemount
-#include "freemount/event_loop.hh"
-#include "freemount/queue_utils.hh"
-#include "freemount/receiver.hh"
-#include "freemount/send_queue.hh"
-
 // freemount-client
 #include "freemount/address.hh"
+#include "freemount/synced.hh"
 
 
 using namespace freemount;
@@ -35,108 +30,10 @@ static int protocol_in  = -1;
 static int protocol_out = -1;
 
 
-struct request_status
-{
-	ssize_t result;
-};
-
 static
 void report_error( const char* path, uint32_t err )
 {
 	more::perror( "fset", path, err );
-}
-
-static inline
-void queue_request( send_queue& queue, uint8_t request_type )
-{
-	queue_int( queue, Frame_request, request_type );
-}
-
-static inline
-void queue_submit( send_queue& queue )
-{
-	queue_empty( queue, Frame_submit );
-}
-
-static
-void send_put_request( int fd, const char* path, const char* data, size_t size )
-{
-	send_queue queue( fd );
-	
-	queue_request( queue, req_write );
-	
-	queue_string( queue, Frame_arg_path,  path, strlen( path ) );
-	queue_string( queue, Frame_send_data, data, size           );
-	
-	queue_submit( queue );
-	
-	queue.flush();
-}
-
-static
-int wait_for_result( int fd, frame_handler_function handler, const char* path )
-{
-	request_status req;
-	
-	data_receiver r( handler, &req );
-	
-	int looped = run_event_loop( r, fd );
-	
-	if ( looped < 0  ||  (looped == 0  &&  (looped = -ECONNRESET)) )
-	{
-		more::perror( "fset", path, -looped );
-		
-		exit( 1 );
-	}
-	
-	if ( looped > 2 )
-	{
-		abort();
-	}
-	
-	int result = req.result;
-	
-	if ( looped == 2 )
-	{
-		result = -result;  // errno
-	}
-	
-	return result;
-}
-
-static
-int frame_handler( void* that, const frame_header& frame )
-{
-	if ( frame.type == Frame_ack_write )
-	{
-		return 0;
-	}
-	
-	int result = ((request_status*) that)->result = get_u32( frame );
-	
-	if ( frame.type == Frame_result )
-	{
-		return result == 0 ? 1 : 2;
-	}
-	
-	return 3;
-}
-
-static
-ssize_t request_put( const char* path, const char* data, size_t length )
-{
-	send_put_request( protocol_out, path, data, length );
-	
-	ssize_t n = wait_for_result( protocol_in, &frame_handler, path );
-	
-	if ( n < 0 )
-	{
-		report_error( path, -n );
-		
-		exit( 1 );
-	}
-	
-	return n;
 }
 
 int main( int argc, char** argv )
@@ -167,13 +64,22 @@ int main( int argc, char** argv )
 		path = "/";
 	}
 	
+	size_t path_len = strlen( path );
+	
 	char* data = argv[ 2 ];
 	
 	size_t len = strlen( data );
 	
 	data[ len++ ] = '\n';
 	
-	request_put( path, data, len );
+	try
+	{
+		synced_put( protocol_in, protocol_out, path, path_len, data, len );
+	}
+	catch ( const path_error& e )
+	{
+		report_error( e.path.c_str(), e.error );
+	}
 	
 	return 0;
 }
