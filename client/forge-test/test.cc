c44fb15f47efa23aa9d83ae43898681ff6351758
Josh Juran <jjuran@metamage.com>
2016-10-02 22:07:44 -0400

    forge-test:  Add a framed caption

--- client/forge-test/test.cc
+++ client/forge-test/test.cc
@@ -41,6 +41,9 @@ static int next_fd = 3;
 #define PUT( path, data )  \
 	synced_put( protocol_in, protocol_out, STR_LEN( path ), STR_LEN( data ) )
 
+#define LINK( src, dst )  \
+	synced_link( protocol_in, protocol_out, STR_LEN( src ), STR_LEN( dst ) )
+
 int main( int argc, char** argv )
 {
 	if ( argc < 2 )
@@ -69,6 +72,12 @@ int main( int argc, char** argv )
 		int window_fd = OPEN( TEST_PORT "/window" );
 		
 		PUT( TEST_PORT "/w/.~title", "Test" );
+		
+		LINK( "/gui/new/frame",   TEST_PORT "/view"   );
+		LINK( "/gui/new/caption", TEST_PORT "/v/view" );
+		
+		PUT( TEST_PORT "/v/padding", "4"           "\n" );
+		PUT( TEST_PORT "/v/v/text",  "Hello world" "\n" );
 	}
 	catch ( const path_error& e )
 	{
