1a4f20d7701283e0a45d6fc9f231f9d820b41e63
Josh Juran <jjuran@metamage.com>
2016-12-17 15:21:31 -0500

    about-forge:  Report malformed addresses

--- client/about-forge/about-forge.cc
+++ client/about-forge/about-forge.cc
@@ -17,11 +17,13 @@
 #include "freemount/synced.hh"
 
 
+#define PROGRAM  "about-forge"
+
 #define PORT  "/gui/port/about"
 
 #define STR_LEN( s )  "" s, (sizeof s - 1)
 
-#define USAGE  "usage: about-forge <Freemount address>\n"
+#define USAGE  "usage: " PROGRAM " <Freemount address>\n"
 
 
 using namespace freemount;
@@ -59,6 +61,7 @@ int main( int argc, char** argv )
 	
 	if ( connector_argv == NULL )
 	{
+		write( STDERR_FILENO, STR_LEN( PROGRAM ": malformed address\n" ) );
 		return 2;
 	}
 	
