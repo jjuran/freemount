efb25cad337303227b232b9bfebeaa8b1621df47
Josh Juran <jjuran@metamage.com>
2026-01-06 23:12:30 -0500

    display:  Silence integer narrowing warni^Werrors

--- client/display/display.cc
+++ client/display/display.cc
@@ -440,8 +440,8 @@ int main( int argc, char** argv )
 		little_endian = false;
 	}
 	
-	short raster_size[ 2 ] = { desc.height, desc.width };
-	short window_size[ 2 ] = { desc.height, desc.width };
+	short raster_size[ 2 ] = { (short) desc.height, (short) desc.width };
+	short window_size[ 2 ] = { (short) desc.height, (short) desc.width };
 	
 	if ( x_numerator > 1  &&  x_numerator <= 16 )
 	{
